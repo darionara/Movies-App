9655d82cdf4e59c299a9450f10a81862e42389e6
Daria Ziobrowska <darianocon@gmail.com>
2023-09-23 20:26:30 +0200

    [feat] Modify slider component

--- apps/client/ui/Range/Range.stories.tsx
+++ apps/client/ui/Range/Range.stories.tsx
@@ -1,23 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { CustomSlider } from './Slider'
+import { CustomRange } from './Range'
 
 const meta = {
-  title: 'Components/Sliders/Slider',
-  component: CustomSlider,
+  title: 'Components/Sliders/Range',
+  component: CustomRange,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-} satisfies Meta<typeof CustomSlider>
+} satisfies Meta<typeof CustomRange>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Slider: Story = {
+export const Range: Story = {
   args: {
     min: 0,
-    max: 500,
-    step: 50
+    max: 10,
+    step: 1
   },
 }
 
--- apps/client/ui/Slider/Slider.tsx
+++ apps/client/ui/Slider/Slider.tsx
@@ -7,42 +7,37 @@ type CustomSliderProps = {
   min?: number
   max?: number
   step?: number
-  defaultValue?: number[]
+  defaultValue?: number
   className?: string
 };
 
 export const CustomSlider: FC<CustomSliderProps> = ({
   min = 0,
-  max = 10,
-  step = 1,
-  defaultValue = [0, 10],
+  max = 500,
+  step = 50,
+  defaultValue = 250,
   className,
   ...props
 }) => {
-  const [values, setValues] = useState<number[]>(defaultValue);
+  const [value, setValue] = useState<number>(defaultValue);
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const handleOnChange = (newValues: number[]) => {
-    setValues(newValues);
+  const handleOnChange = (newValue: number) => {
+    setValue(newValue);
   };
 
   const handleTooltipPosition = () => {
     if (!containerRef.current) return;
 
-    const [leftHandle, rightHandle] = Array.from(containerRef.current.getElementsByClassName('rc-slider-handle')) as HTMLElement[];
-    const [leftTooltip, rightTooltip] = Array.from(containerRef.current.getElementsByClassName('slider-tooltip')) as HTMLElement[];
-
-    if (leftHandle && leftTooltip && rightHandle && rightTooltip) {
+    const handle = containerRef.current?.getElementsByClassName('rc-slider-handle')[0] as HTMLElement | null;
+    const tooltip = containerRef.current?.getElementsByClassName('slider-tooltip')[0] as HTMLElement | null;
+    
+    if (handle && tooltip) {
       const containerRect = containerRef.current.getBoundingClientRect();
-      const leftHandleRect = leftHandle.getBoundingClientRect();
-      const rightHandleRect = rightHandle.getBoundingClientRect();
-      const rightTooltipRect = rightTooltip.getBoundingClientRect();
+      const handleRect = handle.getBoundingClientRect();
 
-      const leftOffset = leftHandleRect.left - containerRect.left - leftHandleRect.width;
-      leftTooltip.style.left = `${leftOffset}px`;
-
-      const rightOffset = rightHandleRect.left - containerRect.left + rightHandleRect.width - rightTooltipRect.width;
-      rightTooltip.style.left = `${rightOffset}px`;
+      const offset = handleRect.left - containerRect.left - handleRect.width;
+      tooltip.style.left = `${offset}px`;
     }
   };
 
@@ -52,43 +47,35 @@ export const CustomSlider: FC<CustomSliderProps> = ({
     return () => {
       window.removeEventListener('resize', handleTooltipPosition);
     };
-  }, [values]);
+  }, [value]);
 
   return (
-    <div className='min-w-[180px] m-5 slider-container' ref={containerRef} {...props}>
+    <div className='min-w-[180px] mt-12 mx-6 mb-0 h-9' ref={containerRef} {...props}>
       <Slider
-        range
         min={min}
         max={max}
         step={step}
-        allowCross={false}
-        defaultValue={values}
+        defaultValue={value}
         onChange={handleOnChange}
-        trackStyle={[{ backgroundColor: 'var(--primary-color)', height: '4px' }]}
+        trackStyle={{ backgroundColor: 'var(--primary-color)', height: '4px' }}
         railStyle={{ backgroundColor: 'var(--dark-grey)', height: '4px' }}
-        handleStyle={[
+        handleStyle={
           {
             backgroundColor: 'var(--primary-color)',
             height: '14px',
             width: '14px',
             border: 'none',
-          },
-          {
-            backgroundColor: 'var(--primary-color)',
-            height: '14px',
-            width: '14px',
-            border: 'none',
-          },
-        ]}
-        className='w-full p-10'
+          }
+        }
+        className='w-full'
       />
-      <div className='slider-tooltip left-tooltip'>
-        <span className="rc-slider-tooltip-arrow"></span>
-        {values[0]}
+      <div className="flex mt-[5px] justify-between">
+        <div className="flex-1 text-left ml-[-5px] text-sm text-gray-500">{min}</div>
+        <div className="flex-1 text-right mr-[-7px] text-sm text-gray-500">{max}</div>
       </div>
-      <div className='slider-tooltip right-tooltip'>
-        <span className="rc-slider-tooltip-arrow"></span>
-        {values[1]}
+      <div className='relative bg-dark-grey py-1.5 px-2 border-0 rounded text-xs z-[1] pointer-events-none text-text-color w-[45px] text-center shadow shadow-[#141417] mt-[-75px] ml-[-1px] slider-tooltip'>
+        <span className="absolute w-0 h-0 border-transparent border-solid border-t-dark-grey bottom-[-4px] left-[50%] ml-[-4px] border-t-4 border-x-4 border-b-0"></span>
+        {value}
       </div>
     </div>
   );
