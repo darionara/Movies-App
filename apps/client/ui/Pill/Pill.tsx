7c65070f5b6e10d139cab83efd696d5b23b6ee2f
Daria Ziobrowska <darianocon@gmail.com>
2023-10-11 18:13:06 +0200

    Make GenrePill component controlled by GenresList

--- apps/client/ui/Pill/Pill.tsx
+++ apps/client/ui/Pill/Pill.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react';
-import type { ComponentPropsWithoutRef, FC } from 'react';
-import clsx from 'clsx';
+import type { ComponentPropsWithoutRef, FC } from 'react'
+import clsx from 'clsx'
 
-type PillProps = ComponentPropsWithoutRef<'button'>
+type PillProps = ComponentPropsWithoutRef<'button'> & {
+  isActive?: boolean
+  onClick?: () => void
+}
 
-export const GenrePill: FC<PillProps> = ({ className, ...props}) => {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
-
+export const GenrePill: FC<PillProps> = ({
+  className,
+  isActive = false,
+  onClick,
+  ...props
+}) => {
   const pillClasses = clsx(
-    'py-1 px-3.5 items-center text-xs text-text-color font-semibold leading-5 capitalize whitespace-nowrap rounded-full border transition-colors duration-100 transition-ease-in-out',
+    'py-1 px-3.5 items-center',
+    'text-xs text-text-color font-semibold leading-5 capitalize',
+    'whitespace-nowrap rounded-full border',
+    'transition-colors duration-100 transition-ease-in-out',
     {
-      'bg-primary border-primary hover:bg-primary hover:border-primary': isActive,
+      'bg-primary border-primary hover:bg-primary hover:border-primary':
+        isActive,
       'bg-black-grey border-white/50 hover:bg-white/[.15]': !isActive,
-    }, 
-    className
-  );
+    },
+    className,
+  )
 
-  return (
-    <button
-      className={pillClasses}
-      onClick={handleClick}
-      {...props}
-    />
-  );
-};
-
-
-
-
-
+  return <button className={pillClasses} onClick={onClick} {...props} />
+}
--- apps/client/ui/PillsList/PillsList.tsx
+++ apps/client/ui/PillsList/PillsList.tsx
@@ -1,24 +1,61 @@
+import { useState } from 'react'
 import type { FC } from 'react'
 import { GenrePill } from '@/ui/Pill/Pill'
 import clsx from 'clsx'
 
-const genres: string[] = ['action', 'adventure', 'animation', 'comedy', 'crime', 'documentary', 'drama', 'family', 'fantasy', 'history', 'horror', 'music', 'mystery', 'romance', 'science fiction', 'TV movie', 'thriller', 'war', 'western']
+const genres: string[] = [
+  'action',
+  'adventure',
+  'animation',
+  'comedy',
+  'crime',
+  'documentary',
+  'drama',
+  'family',
+  'fantasy',
+  'history',
+  'horror',
+  'music',
+  'mystery',
+  'romance',
+  'science fiction',
+  'TV movie',
+  'thriller',
+  'war',
+  'western',
+]
 
 type GenresListProps = {
   className?: string
+  isActive?: boolean
+  onClick?: () => void
 }
 
-export const GenresList: FC<GenresListProps> = (className) => {
+export const GenresList: FC<GenresListProps> = ({ className }) => {
+  const [activeGenres, setActiveGenres] = useState<string[]>([])
 
-  const listClasses = clsx('min-w-[100px] w-full', className)
+  const handleGenreClick = (genre: string) => {
+    setActiveGenres((prevActiveGenres) => {
+      return prevActiveGenres.includes(genre)
+        ? prevActiveGenres.filter((activeGenre) => activeGenre !== genre)
+        : [...prevActiveGenres, genre]
+    })
+  }
 
   return (
-    <div className={listClasses}>
-      <ul className='flex flex-wrap gap-2'>
+    <div className={clsx('min-w-[100px] w-full', className)}>
+      <ul className="flex flex-wrap gap-2">
         {genres.map((genre) => {
           return (
             <li key={genre}>
-              <GenrePill>{genre}</GenrePill>
+              <GenrePill
+                isActive={activeGenres.includes(genre)}
+                onClick={() =>
+                  handleGenreClick(genre)
+                }
+              >
+                {genre}
+              </GenrePill>
             </li>
           )
         })}
