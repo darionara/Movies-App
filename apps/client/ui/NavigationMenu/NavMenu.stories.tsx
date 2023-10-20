cc2fe9801bfcbed988d84db349c0806a2222d382
Daria Ziobrowska <darianocon@gmail.com>
2023-10-20 17:48:30 +0200

    Create NavigationMenu component

---
+++ apps/client/ui/NavigationMenu/NavMenu.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { NavMenu } from '@/ui/NavigationMenu/NavMenu'
+
+const meta = {
+  title: 'Components/Navigation menu',
+  component: NavMenu,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof NavMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Navigation: Story = {
+  args: {},
+}
--- apps/client/ui/NavigationMenu/NavMenu.tsx
+++ apps/client/ui/NavigationMenu/NavMenu.tsx
@@ -1,29 +1,46 @@
 import type { FC } from 'react'
 import clsx from 'clsx'
+import { DropdownMenu } from '@/ui/DropdownMenu/DropdownMenu'
 
 const menuItems = ['TV Shows', 'Movies', 'Series', 'Animations']
 
 type NavMenuProps = {
   className?: string
   activeItem?: string
-  onClick?: (option: string) => void
+  onHover?: (option: string) => void
+  isOpen?: boolean
+  onMouseLeave?: () => void
 }
 
-export const NavMenu: FC<NavMenuProps> = ({ className, activeItem, onClick }) => {
+export const NavMenu: FC<NavMenuProps> = ({
+  className,
+  activeItem,
+  onHover,
+  isOpen,
+  onMouseLeave
+}) => {
   return (
     <ul className={clsx('flex flex-row gap-8', className)}>
       {menuItems.map((item) => (
-        <li
-          key={item}
-          className={clsx('text-lg font-bold capitalize cursor-pointer', {
-            'text-text-color': activeItem === item,
-            'text-text-color/[.50]': activeItem !== item,
-          })}
-          onClick={() => onClick(item)}
-        >
-          {item}
-        </li>
+        <div>
+          <li
+            key={item}
+            className={clsx(
+              'relative text-lg font-bold capitalize cursor-default whitespace-nowrap',
+              'hover:text-text-color transition-colors transition-ease-in-out duration-100',
+              {
+                'text-text-color cursor-pointer': activeItem === item,
+                'text-text-color/[.50]': activeItem !== item,
+              },
+            )}
+            onMouseOver={() => onHover(item)}
+          >
+            {item}
+          </li>
+          {activeItem === item && <DropdownMenu isOpen={isOpen} onMouseLeave={onMouseLeave} className='absolute top-[105px]' />}
+        </div>
       ))}
+
     </ul>
   )
 }
