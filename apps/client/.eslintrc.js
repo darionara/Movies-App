78ae2853b94dbaa25c14774e20c664abb99bc199
Tomek Skuta <kontakt@tomekskuta.pl>
2023-11-10 12:49:15 +0100

    [chore] Fix eslint config

---
+++ apps/client/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    commonjs: true,
+  },
+  extends: [
+    'turbo',
+    'plugin:storybook/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    babelOptions: {
+      presets: ['next/babel'],
+    },
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 2015,
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/order': ['error', { 'newlines-between': 'always' }],
+  },
+};
